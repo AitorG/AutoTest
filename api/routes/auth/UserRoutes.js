cb505773652c8e4a7d6ae1e53970fc36cff7fc16
zamarrowski <sergiozamarro@hotmail.com>
2016-03-11 20:45:03 +0100

    Add userRoutes: delete and changePassword

--- api/routes/auth/UserRoutes.js
+++ api/routes/auth/UserRoutes.js
@@ -9,14 +9,33 @@ router.post('/', function(req, res, next) {
     password: req.body.password,
     email: req.body.email
   };
-  
+
   UserServices.createUser(user, function(err, doc) {
     if (err) {
       next(err);
     } else {
       res.json(doc);
     }
+  });
+});
 
+router.delete('/:userId', function(req, res, next) {
+  UserServices.deleteUser(req.params.userId, function(err, response) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+router.post('/changePassword', function(req, res, next) {
+  UserServices.changePassword(req.body.userId, req.body.newPassword, req.body.oldPassword, function(err, doc) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(doc);
+    }
   });
 });
 
